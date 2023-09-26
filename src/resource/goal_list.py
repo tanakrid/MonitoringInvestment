0751f091f93b00dbd868a4b0ca9bbb08b45b3100
tanakrid chanburi <tanakrid.c@ku.th>
2023-09-26 14:57:28 +0700

    add not found case

--- src/resource/goal_list.py
+++ src/resource/goal_list.py
@@ -3,7 +3,7 @@ from ..util import constant
 
 import json
 from flask_restful import Resource, marshal_with, fields
-from flask import jsonify, request
+from flask import jsonify, request, abort
 
 goal_list = {}
 goal_list_fields = {}
@@ -27,27 +27,38 @@ class GoalList(Resource):
         count += 1
         goal_list[tmp.id] = tmp
         goal_list_fields[tmp.id] = fields.Nested(goal_fields)
-        return
+        return 
     
 class Goal(Resource):
 
     @marshal_with(goal_fields)
     def get(self, id):
-        return goal_list[int(id)]
+        try:
+            return goal_list[int(id)]
+        except:
+            return abort(404)
     
     def patch(self, id):
-        data = json.loads(request.data)
-        tmp = GoalModel(
-            data[constant.goal_name], 
-            data[constant.goal_target], 
-            data[constant.goal_start_date], 
-            data[constant.goal_end_date])
-        tmp.id = id
-        goal_list[int(id)] = tmp
+        try:
+            goal_list[int(id)] # for check what this goal is still in goal list
+            data = json.loads(request.data)
+            tmp = GoalModel(
+                data[constant.goal_name], 
+                data[constant.goal_target], 
+                data[constant.goal_start_date], 
+                data[constant.goal_end_date])
+            tmp.id = id
+            goal_list[int(id)] = tmp
+        except:
+            return abort(404)
         return
     
     def delete(self, id):
-        goal_list.pop(int(id))
-        goal_list_fields.pop(int(id))
+        try:
+            goal_list[int(id)] # for check what this goal is still in goal list
+            goal_list.pop(int(id))
+            goal_list_fields.pop(int(id))
+        except:
+            return abort(404)
         return
 
