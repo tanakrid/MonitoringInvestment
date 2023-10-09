867e4cef1ebf1a0fb91bfd77f84706241f9dd487
tanakrid chanburi <tanakrid.c@ku.th>
2023-10-09 21:59:36 +0700

    Re-design class path

--- app.py
+++ app.py
@@ -1,35 +1,15 @@
-# from project import create_app
+from project import create_app
+import config
+import os
+import sys
 
-# app = create_app()
+# Get the absolute path of the project's root directory
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__)))
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+# Add the project root to sys.path
+sys.path.insert(0, root_dir)
 
+app = create_app()
 
-from sqlalchemy import create_engine
-
-# Replace 'mysecretpassword' with the password you set in Step 3
-
-
-db_host = 'localhost'
-db_port = '5432'
-db_name = 'postgres'
-db_user = 'postgres'
-db_password = 'lwpjtylla6EhcZ8r5Gmu'
-db_endpoint = 'sit-pfm-db.cyrgkvmfx5v1.ap-southeast-1.rds.amazonaws.com'
-
-# Create the database URL for SQLAlchemy
-db_url = f'postgresql://{db_user}:{db_password}@{db_endpoint}:{db_port}/{db_name}'
-print(db_url)
-
-# Create the SQLAlchemy engine
-engine = create_engine(db_url)
-
-# Test the connection
-connection = engine.connect()
-result = connection.execute('SELECT 1')
-for row in result:
-    print(row[0])
-
-# Close the connection
-connection.close()
+if __name__ == '__main__':
+    app.run(debug=True)
