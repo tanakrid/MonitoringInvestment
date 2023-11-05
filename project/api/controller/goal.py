59cc5fd2296b273bd07e2d370f7ab65d8d1c498d
tanakrid chanburi <tanakrid.c@ku.th>
2023-11-05 23:25:18 +0700

    Add Controller Layer, goal controller

--- project/api/controller/goal.py
+++ project/api/controller/goal.py
@@ -1,5 +1,7 @@
-from . import constant
+from ..util import constant
 from project.api.model.goal import Goal as GoalModel
+from project.api.model.user import User
+import traceback 
 
 def validate_input_post_goal(args):
 
@@ -11,5 +13,8 @@ def validate_input_post_goal(args):
             target=args[constant.goal_target], 
             start_date=args[constant.goal_start_date], 
             end_date=args[constant.goal_end_date],
-            progress= 0
+            progress= 0,
+            create_date=args[constant.goal_start_date],
+            user_id = args['user_id'],
+            user = User.query.filter_by(id=args['user_id']).first()
         )
